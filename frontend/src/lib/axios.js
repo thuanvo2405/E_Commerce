39ebfa338e4e6cc5fb7d887f9e58338d619e7ce6
thuanvo2405 <vothuan2405003@gmail.com>
2025-05-05 12:59:10 +0700

    Fix

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,6 +2,8 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   baseURL:
-    import.meta.mode === "development" ? "http://localhost:3000/api" : "/api",
+    import.meta.env.MODE === "development"
+      ? "http://localhost:3000/api"
+      : "/api",
   withCredentials: true,
 });
